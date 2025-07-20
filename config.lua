299c84eddc027dc389135bfdb9cefb0886f6785b
Josh <57309415+Obtaizen@users.noreply.github.com>
2025-07-20 23:13:59 +0100

    [songs]
    removed a bunch of songs
    one per station now so easier to add new songs without removing old songs and breaking it

--- config.lua
+++ config.lua
@@ -1,18 +1,6 @@
 SongInfo = {
     ['6000'] = { 'Look To Windward', 'Sleep Token' },
-    ['6001'] = { 'Emergence', 'Sleep Token' },
-    ['6002'] = { 'Past Self', 'Sleep Token' },
-    ['6003'] = { 'Dangerous', 'Sleep Token' },
-    ['6004'] = { 'Caramel', 'Sleep Token' },
-    ['6005'] = { 'Even In Arcadia', 'Sleep Token' },
-    ['6006'] = { 'Provider', 'Sleep Token' },
-    ['6007'] = { 'Damocles', 'Sleep Token' },
-    ['6008'] = { 'Gethsemane', 'Sleep Token' },
-    ['6009'] = { 'Infinite Baths', 'Sleep Token' },
     ['6010'] = { 'Bad Habits', 'KUURO' },
-    ['6011'] = { 'Empty Bed', 'Saint Punk' },
-    ['6012'] = { 'Big Bands', 'Lil Cap, P Money' },
-    ['6013'] = { 'Nightshift', 'Charlotte Plank' },
 }
 
 CustomStations = {
